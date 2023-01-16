c86362a1d9eb763ef64d97fbd01c375c2832193e
ppizarror <pablo@ppizarror.com>
2023-01-16 01:11:35 +0000

    Update build

--- MLStructFP/__init__.py
+++ MLStructFP/__init__.py
@@ -9,7 +9,7 @@ __author__ = 'Pablo Pizarro R.'
 __description__ = 'Machine learning structural floor plan dataset'
 __keywords__ = ['ml', 'ai', 'dataset', 'calc', 'matrix analysis', 'cnn', 'structural analysis', 'structural design']
 __email__ = 'pablo@ppizarror.com'
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 
 # URL
 __url__ = 'https://github.com/MLSTRUCT/MLSTRUCT_FP'
--- build.py
+++ build.py
@@ -13,7 +13,7 @@ mode = sys.argv[1].strip()
 if mode == 'pip':
     if os.path.isdir('dist'):
         for k in os.listdir('dist'):
-            if 'pygame_menu-' in k or 'pygame-menu-' in k:
+            if 'MLStructFP-' in k:
                 os.remove(f'dist/{k}')
     if os.path.isdir('build'):
         for k in os.listdir('build'):
