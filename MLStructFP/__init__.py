c35fc44577e33c16842af725f88b820c64b25cca
ppizarror <pablo@ppizarror.com>
2023-11-19 18:07:12 -0300

    Update URL

--- MLStructFP/__init__.py
+++ MLStructFP/__init__.py
@@ -9,10 +9,10 @@ __author__ = 'Pablo Pizarro R.'
 __description__ = 'Machine learning structural floor plan dataset'
 __keywords__ = ['ml', 'ai', 'dataset', 'calc', 'matrix analysis', 'cnn', 'structural analysis', 'structural design']
 __email__ = 'pablo@ppizarror.com'
-__version__ = '0.3.4'
+__version__ = '0.3.5'
 
 # URL
-__url__ = 'https://github.com/MLSTRUCT/MLSTRUCT_FP'
-__url_bug_tracker__ = 'https://github.com/MLSTRUCT/MLSTRUCT_FP/issues'
-__url_documentation__ = 'https://github.com/MLSTRUCT/MLSTRUCT_FP'
-__url_source_code__ = 'https://github.com/MLSTRUCT/MLSTRUCT_FP'
+__url__ = 'https://github.com/MLSTRUCT/MLSTRUCT-FP'
+__url_bug_tracker__ = 'https://github.com/MLSTRUCT/MLSTRUCT-FP/issues'
+__url_documentation__ = 'https://github.com/MLSTRUCT/MLSTRUCT-FP'
+__url_source_code__ = 'https://github.com/MLSTRUCT/MLSTRUCT-FP'
