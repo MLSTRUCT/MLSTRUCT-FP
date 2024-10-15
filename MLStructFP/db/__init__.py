7c6cde69565859b4778f5657f9404161373a4821
Pablo Pizarro R. <pablo@ppizarror.com>
2024-10-14 22:05:32 -0300

    Expose types

--- MLStructFP/db/__init__.py
+++ MLStructFP/db/__init__.py
@@ -6,3 +6,9 @@ Dataset load and objects.
 
 from MLStructFP.db._db_loader import DbLoader
 from MLStructFP.db._floor import Floor
+
+from MLStructFP.db._c_item import Item
+from MLStructFP.db._c_point import Point
+from MLStructFP.db._c_rect import Rect
+from MLStructFP.db._c_room import Room
+from MLStructFP.db._c_slab import Slab
